f78272cb077bff63d43939581963b3eeebffd95d
hexist <hexist666@gmail.com>
2018-10-20 18:38:18 +0300

    Initial commit

---
+++ src/main/java/com/gusev/spring5quizapp/Spring5QuizAppApplication.java
@@ -0,0 +1,12 @@
+package com.gusev.spring5quizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5QuizAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5QuizAppApplication.class, args);
+    }
+}
---
+++ src/test/java/com/gusev/spring5quizapp/Spring5QuizAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gusev.spring5quizapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5QuizAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
